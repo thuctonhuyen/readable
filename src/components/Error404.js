907cd8422dc46f8311a7433e546da56f4776412a
thuctonhuyen <thuctonhuyen@gmail.com>
2017-09-06 22:00:10 -0700

    feat: Add html for error 404

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,35 +1,19 @@
 import React, {Component} from 'react';
 import '../css/App.css';
-import NavigationBar from './NavigationBar';
-import ListPosts from './ListPosts'
-import {Grid, Row} from 'react-bootstrap'
-import {Route} from 'react-router-dom'
-import NewPost from './NewPost'
-import PostDetail from './PostDetail'
-import {connect} from 'react-redux'
-import {fetchAllPosts} from '../actions/posts_actions';
-import {fetchAllCategories} from '../actions/categories_actions';
-import {withRouter} from 'react-router-dom'
+import {Jumbotron}from 'react-bootstrap'
 
-class Component404 extends Component {
+class Error404 extends Component {
 
 
     render() {
         return (
-            <Grid className="App">
-                <Row>
-                    <NavigationBar/>
-                </Row>
-
-                {["/:category", "/"].map((path, index) => <Route key={index} exact path={path} component={ListPosts}/>)}
-                <Route exact path="/add/newPost" component={NewPost}/>
-                <Route exact path="/:category/:id" component={PostDetail}/>
-
-
-            </Grid>
+            <Jumbotron>
+                <h1>URL NOT FOUND</h1>
+                <p>We are sorry but the page you are looking for does not exist.</p>
+            </Jumbotron>
         );
     }
 }
 
 
-export default (Component404);
+export default (Error404);
