1ba37f5d6f466577b1e5e29ab72260c5718e2a3e
thuctonhuyen <thuctonhuyen@gmail.com>
2017-09-06 21:59:21 -0700

    add 404 component

---
+++ src/components/Error404.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import '../css/App.css';
+import NavigationBar from './NavigationBar';
+import ListPosts from './ListPosts'
+import {Grid, Row} from 'react-bootstrap'
+import {Route} from 'react-router-dom'
+import NewPost from './NewPost'
+import PostDetail from './PostDetail'
+import {connect} from 'react-redux'
+import {fetchAllPosts} from '../actions/posts_actions';
+import {fetchAllCategories} from '../actions/categories_actions';
+import {withRouter} from 'react-router-dom'
+
+class Component404 extends Component {
+
+
+    render() {
+        return (
+            <Grid className="App">
+                <Row>
+                    <NavigationBar/>
+                </Row>
+
+                {["/:category", "/"].map((path, index) => <Route key={index} exact path={path} component={ListPosts}/>)}
+                <Route exact path="/add/newPost" component={NewPost}/>
+                <Route exact path="/:category/:id" component={PostDetail}/>
+
+
+            </Grid>
+        );
+    }
+}
+
+
+export default (Component404);
